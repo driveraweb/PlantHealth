f26b14df486a1009600ddfda3611ac380d6b3b2b
driveraweb <driveraweb@gmail.com>
2018-04-23 15:43:43 -0600

    Installation file - incomplete

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setuptools
+
+
+setup(name='planthealth',
+      version='0.1.0',
+      description='NDVI plant health Raspberry Pi project',
+      url='ece.nmsu.edu',
+      author='Jessi Jo Gonzales, '
+              + 'Ryan Levendosky, '
+              + 'Jose Olivarez, '
+              + 'Derrick Rivera'
+      author_email='driveraweb@gmail.com',
+      license='NMSU',
+      packages=['bin', 'docs', 'planthealth'],
+      )
