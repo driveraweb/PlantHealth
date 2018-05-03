99a0a9ce2141cc7a52913a6f4ecd8713ac2ec14b
driveraweb <driveraweb@gmail.com>
2018-05-03 02:19:38 -0600

    NDVI functions, calculations, and sample images

--- planthealth/core.py
+++ planthealth/core.py
@@ -1,14 +1,44 @@
 import numpy as np
-from PIL import Image
+import cv2
 
 ### FUNCTIONS USED FOR NDVI IMAGE ANALYSIS AND USER INTERFACE ###
 
-# Parameters:  img as numpy array
-# Returns:  rank 3 numpy array with R,G, and N as the pixel values
-#           for colors red, green, and near-infrared as floats
-def decompose(img):    
-    img.convert('RGB')
-    arr = np.array(img).astype(float)
+# colormap()
+# Parameters:  img as numpy array having NDVI values in range [-1,1]
+# Returns:  numpy array with R,G, and B color mapped values  
+#   corresponding NDVI values
+def colormap(red, nir):    
+    red = red.astype(float)
+    nir = nir.astype(float)
     
-    return [arr, size]
+    denom = (nir+red)
+    denom[denom<=0] = 0.0001
+    ndvi = (nir-red)/denom
+    #ndvi = (ndvi- np.min(ndvi)) 
+    ndvi = ndvi / np.max(ndvi) #normalize
+    print(np.min(ndvi), np.max(ndvi))
+    ndvi = ndvi*255 #0 to 255
+    
+    ndvi = np.around(ndvi).astype('uint8') #round and convert to int
+    
+
+    #print(ndvi.shape)
+    
+    #ndvi_bgr[bgr] = ndvi
+    #ndvi_gry[gry] = ndvi
+    
+    #ndvi_bgr = cv2.applyColorMap(ndvi_bgr, cv2.COLORMAP_JET)
+    #ndvi_gry = cv2.applyColorMap(ndvi_gry, cv2.COLORMAP_BONE)
+    
+    
+    #ret, ndvi = cv2.threshold(ndvi, 127, 255, cv2.THRESH_TOZERO)
+    
+    #ndvi = cv2.applyColorMap(ndvi, cv2.COLORMAP_JET)
+    
+    
+    ndvi = cv2.applyColorMap(ndvi, cv2.COLORMAP_JET)
+    return ndvi
+ 
+
+
     
---
+++ planthealth/utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+import scipy
+
+# channelSplit()
+# to get BGR values from a cv2 image
+def channelSplit(image):
+    return np.dsplit(image,image.shape[-1])
+
+# showImage()
+# shows img for t time (in ms)
+#
+def showImage(img, t):
+    img = cv2.resize(img, (1820, 1368))
+    cv2.imshow('Frame', img)
+    cv2.waitKey(t)
+    cv2.destroyAllWindows()
+    
+    return
+    
+# makeLUT()
+# create lookup table for use with cv2 based on colormap
+# for PlantHealthDetection NDVI 
+def makeLUT():
+    r = np.zeros((256),dtype='float')
+    g = np.zeros((256),dtype='float')
+    b = np.zeros((256),dtype='float')
+    
+    #set up initial BW color values
+    for i in range(0,127):
+        r[i] = (i*1.2)
+        g[i] = (i*1.2)
+        b[i] = (i*1.2)
+    
+    #set up initial RGB color values
+    cVals[136] = (198, 65, 255) #violet
+    cVals[164] = (255, 65, 65) #red
+    cVals[182] = (255, 248, 65) #yellow
+    cVals[212] = (65, 255, 68)#green
+    cVals[255] = (65, 147, 255) #blue
+    # #interpolate missing values
+    scipy.interpolate.RegularGridInterpolator(cVals[127:255],)
+    
+    
+    return r
