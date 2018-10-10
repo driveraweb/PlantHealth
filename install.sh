e96657be2c180755586804699980ef145e5626bc
Derrick Rivera <driveraweb@gmail.com>
2018-10-10 00:20:20 -0600

    dev packges

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo apt install python3 gcc g++ python3-pip build-essential libgtk-3-dev libglw1-mesa libglw1-mesa-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev python-wxtools jupyter-notebook
+sudo apt install python3 gcc g++ python3-pip build-essential libgtk-3-dev libglw1-mesa libglw1-mesa-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev python-wxtools jupyter-notebook libblas-dev libblas3 libopencv-dev 
