d064bcd7fe3d22befd08f3d28ba488a6d34948b1
Derrick <driveraweb@gmail.com>
2018-11-12 13:13:52 -0700

    openBLAS for numpy

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo apt install python3 gcc g++ python3-pip build-essential libgtk-3-dev libglw1-mesa libglw1-mesa-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev python-wxtools libblas-dev libblas3 libopencv-dev libeigen3-dev python-smbus
+sudo apt install python3 gcc g++ python3-pip build-essential libgtk-3-dev libglw1-mesa libglw1-mesa-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev python-wxtools libblas-dev libblas3 libopencv-dev libeigen3-dev python-smbus libopenblas-dev
 
