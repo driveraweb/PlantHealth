260b156498f8d5d0d30b73963aaa6452dd1c8bd7
Derrick <driveraweb@gmail.com>
2018-11-13 13:11:21 -0700

    camera capture test

--- planthealth/main.py
+++ planthealth/main.py
@@ -1,13 +1,38 @@
 import argparse
 import wx
+import picamera
+import sys
+sys.path.append('../../Downloads/ivport-v2')
+import ivport
+from config import *
+import core
 
 def main(args):
-    #code for main program
     print("Main file running")
-    app = wx.App()
-    frame = wx.Frame(None, title='Simple Application')
-    frame.Show()
-    app.MainLoop()
+    #initialize cameras
+    camera = picamera.PiCamera()
+    camera.resolution = (1920,1088)
+    camera.framerate = 1
+    iv = ivport.IVPort(ivport.TYPE_QUAD2, iv_jumper='A')
+    
+    print('Switching to RGB cam')
+    iv.camera_change(2) #start at RGB camera
+    print('Capturing RGB image')
+    imRef = core.snapshot(camera)
+    print('Switching to NoIR cam')
+    iv.camera_change(1) #get NIR image
+    print('Capturing NIR image')
+    imNIR = core.snapshot(camera)
+    print('Processing...')
+    core.process_snapshot(imNIR, imRef)
+    
+    #code for main program
+
+    #app = wx.App()
+    #frame = wx.Frame(None, title='Simple Application')
+    #frame.Show()
+    # app.MainLoop()
+    
 
 
 if __name__ == '__main__':
